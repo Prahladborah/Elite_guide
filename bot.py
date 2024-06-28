df8ea80ff4fd82880870b1616386b852bdbb378f
Prahladborah <86541744+Prahladborah@users.noreply.github.com>
2024-06-28 11:31:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,9 +80,6 @@ async def on_message(message):
         except ValueError:
             await message.channel.send("Please provide a valid level number after the command.")
 
-await bot.process_commands(message)
-    
-
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
